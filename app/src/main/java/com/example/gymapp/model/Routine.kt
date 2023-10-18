9627055fe8a5df6b6723bab828f71f8aac056a9f
Maciej Bąk <maciejbak2003@gmail.com>
2023-10-19 01:27:18 +0200

    added scroll list view, adding and deleting exercises
    user can scroll past exercises, add and delete exercises

--- app/src/main/java/com/example/gymapp/model/Routine.kt
+++ app/src/main/java/com/example/gymapp/model/Routine.kt
@@ -5,13 +5,13 @@ import kotlin.time.Duration
 
 
 data class Routine(
-    var exerciseName: String,
+    var exerciseName: String?,
     //val progression: EnumSet<ProgressionType>,
-    val pause: Duration,
-    val load: Weight,
-    val series: Int,
-    val reps: Reps,
-    val rpe: Rpe,
-    val pace: Pace
+    val pause: Duration?,
+    val load: Weight?,
+    val series: Int?,
+    val reps: Reps?,
+    val rpe: Rpe?,
+    val pace: Pace?
     )
 
