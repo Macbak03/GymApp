c89db1fc7183e1cc5f46f1ef57e841b545f9c5f1
Maciej Bąk <maciejbak2003@gmail.com>
2023-11-10 21:27:14 +0100

    added plan databaseTable

--- app/src/main/java/com/example/gymapp/activity/TrainingPlanActivity.kt
+++ app/src/main/java/com/example/gymapp/activity/TrainingPlanActivity.kt
@@ -3,17 +3,21 @@ package com.example.gymapp.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gymapp.adapter.TrainingPlanRecyclerViewAdapter
 import com.example.gymapp.databinding.ActivityTrainingPlanBinding
+import com.example.gymapp.exception.ValidationException
 import com.example.gymapp.model.trainingPlans.TrainingPlanElement
+import com.example.gymapp.persistence.PlanDataBaseHelper
 
 class TrainingPlanActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrainingPlanBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var trainingPlanRecyclerViewAdapter: TrainingPlanRecyclerViewAdapter
 
+    private val dataBase = PlanDataBaseHelper(this, null)
     private val routines: MutableList<TrainingPlanElement> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +37,44 @@ class TrainingPlanActivity : AppCompatActivity() {
             val explicitIntent = Intent(applicationContext, CreateRoutineActivity::class.java)
             startActivity(explicitIntent)
         }
+
+        binding.buttonSave.setOnClickListener()
+        {
+            try {
+                savePlanIntoDb()
+            } catch (exception: ValidationException)
+            {
+                Toast.makeText(this, exception.message, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.button2.setOnClickListener()
+        {
+            loadPlanFromDB()
+        }
+    }
+
+    private fun savePlanIntoDb()
+    {
+        val planName = binding.editTextTrainingPlanName.text.toString()
+        if(planName.isBlank())
+        {
+            throw ValidationException("Training plan name cannot be empty")
+        }
+        if(trainingPlanRecyclerViewAdapter.itemCount == 0)
+        {
+            throw ValidationException("You must add at least one routine to save a plan")
+        }
+        else
+        {
+            dataBase.addPLan(planName)
+        }
+    }
+
+    private fun loadPlanFromDB()
+    {
+        val cursor = dataBase.getPlans()
+        cursor!!.moveToFirst()
+        cursor.close()
     }
 }
--- app/src/main/java/com/example/gymapp/adapter/TrainingPlanRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/gymapp/adapter/TrainingPlanRecyclerViewAdapter.kt
@@ -7,15 +7,18 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.gymapp.databinding.TrainingPlansRecyclerViewItemLayoutBinding
 import com.example.gymapp.model.trainingPlans.TrainingPlanElement
 
-class TrainingPlanRecyclerViewAdapter(private val trainingPlanElements: MutableList<TrainingPlanElement>): RecyclerView.Adapter<TrainingPlanRecyclerViewAdapter.TrainingPlanViewHolder>(){
+class TrainingPlanRecyclerViewAdapter(private val trainingPlanElements: MutableList<TrainingPlanElement>) :
+    RecyclerView.Adapter<TrainingPlanRecyclerViewAdapter.TrainingPlanViewHolder>() {
 
-    inner class TrainingPlanViewHolder(binding: TrainingPlansRecyclerViewItemLayoutBinding) : ViewHolder(binding.root){
+    inner class TrainingPlanViewHolder(binding: TrainingPlansRecyclerViewItemLayoutBinding) :
+        ViewHolder(binding.root) {
         val trainingPlanName = binding.textViewElement
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrainingPlanViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val trainingPlanItemBinding = TrainingPlansRecyclerViewItemLayoutBinding.inflate(inflater, parent, false)
+        val trainingPlanItemBinding =
+            TrainingPlansRecyclerViewItemLayoutBinding.inflate(inflater, parent, false)
         return TrainingPlanViewHolder(trainingPlanItemBinding)
     }
 
