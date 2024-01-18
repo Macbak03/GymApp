4b6987c2005bc680c0cff6ff03994468e975d7f6
Maciej Bąk <maciejbak2003@gmail.com>
2024-01-18 22:07:27 +0100

    chenged deleting single element design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -85,8 +85,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("it.xabaras.android:recyclerview-swipedecorator:1.4")
 
-    implementation("com.google.android.material:material:1.12.0-alpha02")
+    implementation("com.google.android.material:material:1.12.0-alpha03")
 }
 
 tasks.withType<Test> {
--- app/src/main/java/com/example/gymapp/activity/TrainingPlanActivity.kt
+++ app/src/main/java/com/example/gymapp/activity/TrainingPlanActivity.kt
@@ -2,24 +2,28 @@ package com.example.gymapp.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gymapp.R
 import com.example.gymapp.adapter.TrainingPlanRecyclerViewAdapter
-import com.example.gymapp.animation.Animations
 import com.example.gymapp.databinding.ActivityTrainingPlanBinding
 import com.example.gymapp.fragment.TrainingPlansFragment
 import com.example.gymapp.model.trainingPlans.TrainingPlanElement
 import com.example.gymapp.persistence.PlansDataBaseHelper
 import com.example.gymapp.persistence.RoutinesDataBaseHelper
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 class TrainingPlanActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrainingPlanBinding
@@ -28,13 +32,88 @@ class TrainingPlanActivity : AppCompatActivity() {
 
     private lateinit var slideUpAnimation: Animation
     private lateinit var slideDownAnimation: Animation
-    private val animations: Animations = Animations()
 
     private var planName: String? = null
     private var routines: MutableList<TrainingPlanElement> = ArrayList()
     private val routinesDataBase = RoutinesDataBaseHelper(this, null)
     private val plansDataBase = PlansDataBaseHelper(this, null)
     private val defaultElement = "Create Routine"
+
+    private val simpleCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            if (direction == ItemTouchHelper.RIGHT) {
+                showSingleDeleteDialog(position)
+            }
+        }
+        override fun getSwipeDirs(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            if (trainingPlanRecyclerViewAdapter.isLongClickActivated) {
+                return 0
+            }
+            return super.getSwipeDirs(recyclerView, viewHolder)
+        }
+
+        /*Copyright {2024} {Maciej Bąk}
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.*/
+
+        override fun onChildDraw(
+            c: Canvas,
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            dX: Float,
+            dY: Float,
+            actionState: Int,
+            isCurrentlyActive: Boolean
+        ) {
+            RecyclerViewSwipeDecorator.Builder(
+                c,
+                recyclerView,
+                viewHolder,
+                dX,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+                .addBackgroundColor(
+                    ContextCompat.getColor(
+                        this@TrainingPlanActivity,
+                        R.color.red
+                    )
+                )
+                .addActionIcon(R.drawable.baseline_delete_24)
+                .setActionIconTint(0xFFFFFFFF.toInt())
+                .addCornerRadius(TypedValue.COMPLEX_UNIT_DIP, 3)
+                .addPadding(TypedValue.COMPLEX_UNIT_DIP, 6f, 14f, 6f)
+                .addSwipeRightLabel("Delete")
+                .setSwipeRightLabelColor(0xFFFFFFFF.toInt())
+                .setSwipeRightLabelTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                .create()
+                .decorate()
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        }
+    }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -79,66 +158,59 @@ class TrainingPlanActivity : AppCompatActivity() {
 
         }
         planName = binding.textViewTrainingPlanName.text.toString()
-        val planId = plansDataBase.getPlanId(planName)
-
 
         recyclerView = binding.RecyclerViewTrainingPlan
-        if(planId != null)
+        trainingPlanRecyclerViewAdapter = TrainingPlanRecyclerViewAdapter(
+            routines,
+            binding.toolbarDeleteRoutine.buttonDeleteElements,
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = trainingPlanRecyclerViewAdapter
+
+        setRecyclerViewContent()
+
+        binding.buttonAddRoutine.setOnClickListener()
         {
-            trainingPlanRecyclerViewAdapter = TrainingPlanRecyclerViewAdapter(
-                routines,
-                binding.toolbarDeleteRoutine.buttonDeleteElements,
-                this,
-                routinesDataBase,
-                planId
-            )
-
-            recyclerView.layoutManager = LinearLayoutManager(applicationContext)
-            recyclerView.adapter = trainingPlanRecyclerViewAdapter
-
-            setRecyclerViewContent()
-
-            binding.buttonAddRoutine.setOnClickListener()
-            {
-                val explicitIntent = Intent(applicationContext, CreateRoutineActivity::class.java)
-                explicitIntent.putExtra(PLAN_NAME, planName)
-                startCreateRoutineActivityForResult.launch(explicitIntent)
-            }
-
-            trainingPlanRecyclerViewAdapter.setOnClickListener(object :
-                TrainingPlanRecyclerViewAdapter.OnClickListener {
-                override fun onClick(position: Int, model: TrainingPlanElement) {
-                    val explicitIntent = Intent(applicationContext, CreateRoutineActivity::class.java)
-                    if (routines[0].routineName == defaultElement) {
-                        explicitIntent.putExtra(PLAN_NAME, planName)
-                        startCreateRoutineActivityForResult.launch(explicitIntent)
-                    } else {
-                        explicitIntent.putExtra(ROUTINE_NAME, model.routineName)
-                        explicitIntent.putExtra(PLAN_NAME, planName)
-                        startCreateRoutineActivityForResult.launch(explicitIntent)
-                    }
-                }
-            })
-
-            trainingPlanRecyclerViewAdapter.setOnLongClickListener(object :
-                TrainingPlanRecyclerViewAdapter.OnLongClickListener {
-                override fun onLongClick(position: Int, model: TrainingPlanElement) {
-                    showToolbar()
-                }
-            })
-
-            binding.toolbarDeleteRoutine.buttonBackFromDeleteMode.setOnClickListener {
-                trainingPlanRecyclerViewAdapter.resetLongClickState()
-                hideToolbar()
-            }
-
-            binding.toolbarDeleteRoutine.buttonDeleteElements.setOnClickListener {
-                showDeleteDialog()
-            }
-
-            onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+            val explicitIntent = Intent(applicationContext, CreateRoutineActivity::class.java)
+            explicitIntent.putExtra(PLAN_NAME, planName)
+            startCreateRoutineActivityForResult.launch(explicitIntent)
         }
 
+        trainingPlanRecyclerViewAdapter.setOnClickListener(object :
+            TrainingPlanRecyclerViewAdapter.OnClickListener {
+            override fun onClick(position: Int, model: TrainingPlanElement) {
+                val explicitIntent = Intent(applicationContext, CreateRoutineActivity::class.java)
+                if (routines[0].routineName == defaultElement) {
+                    explicitIntent.putExtra(PLAN_NAME, planName)
+                    startCreateRoutineActivityForResult.launch(explicitIntent)
+                } else {
+                    explicitIntent.putExtra(ROUTINE_NAME, model.routineName)
+                    explicitIntent.putExtra(PLAN_NAME, planName)
+                    startCreateRoutineActivityForResult.launch(explicitIntent)
+                }
+            }
+        })
+
+        trainingPlanRecyclerViewAdapter.setOnLongClickListener(object :
+            TrainingPlanRecyclerViewAdapter.OnLongClickListener {
+            override fun onLongClick(position: Int, model: TrainingPlanElement) {
+                showToolbar()
+            }
+        })
+
+        binding.toolbarDeleteRoutine.buttonBackFromDeleteMode.setOnClickListener {
+            trainingPlanRecyclerViewAdapter.resetLongClickState()
+            hideToolbar()
+        }
+
+        binding.toolbarDeleteRoutine.buttonDeleteElements.setOnClickListener {
+            showDeleteDialog()
+        }
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
 
@@ -167,6 +239,23 @@ class TrainingPlanActivity : AppCompatActivity() {
                     trainingPlanRecyclerViewAdapter.deleteRoutinesFromRecyclerView()
                     val deletedRoutines = trainingPlanRecyclerViewAdapter.getDeletedRoutines()
                     routinesDataBase.deleteRoutines(planId, deletedRoutines)
+                }
+            }
+            this.setNegativeButton("Cancel") { _, _ -> }
+            this.show()
+        }
+    }
+
+    private fun showSingleDeleteDialog(position: Int) {
+        val builder = this.let { AlertDialog.Builder(it) }
+        with(builder) {
+            this.setTitle("Are you sure you want to delete ${routines[position].routineName}?")
+            this.setPositiveButton("Yes") { _, _ ->
+                val planId = plansDataBase.getPlanId(planName)
+                if(planId != null)
+                {
+                    trainingPlanRecyclerViewAdapter.deleteSingleRoutine(position)
+                    routinesDataBase.deleteRoutines(planId, trainingPlanRecyclerViewAdapter.getDeletedRoutines())
                 }
             }
             this.setNegativeButton("Cancel") { _, _ -> }
