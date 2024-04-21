1aa263c97e23c1ba406322706f0a356765c9e728
Maciej Bąk <maciejbak2003@gmail.com>
2024-04-22 00:35:34 +0200

    marker done

--- app/src/main/java/com/example/gymapp/chart/CustomMarkerLabelFormatter.kt
+++ app/src/main/java/com/example/gymapp/chart/CustomMarkerLabelFormatter.kt
@@ -1,20 +1,29 @@
 package com.example.gymapp.chart
 
-import android.graphics.RectF
 import com.patrykandpatrick.vico.core.chart.values.ChartValues
-import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
-import com.patrykandpatrick.vico.core.component.text.TextComponent
-import com.patrykandpatrick.vico.core.context.DrawContext
 import com.patrykandpatrick.vico.core.marker.Marker
 import com.patrykandpatrick.vico.core.marker.MarkerLabelFormatter
 
-class CustomMarker: MarkerLabelFormatter{
+class CustomMarkerLabelFormatter(private val data: List<Float>): MarkerLabelFormatter{
     override fun getLabel(
         markedEntries: List<Marker.EntryModel>,
         chartValues: ChartValues
     ): CharSequence {
-        val builder = StringBuilder()
-        
+        val labelBuilder = StringBuilder()
+        for (entry in markedEntries) {
+            val index = entry.index
+            val displayValue = if (data[index] % 1 == 0f){
+                String.format("%.0f", data[index])
+            } else{
+                String.format("%.2f", data[index])
+            }
+            if(data[index] <= 1f){
+                labelBuilder.append("$displayValue ").append("rep").append("\n")
+            }else{
+                labelBuilder.append("$displayValue ").append("reps").append("\n")
+            }
+        }
+        return labelBuilder.toString()
     }
 
 
--- app/src/main/java/com/example/gymapp/fragment/ChartsFragment.kt
+++ app/src/main/java/com/example/gymapp/fragment/ChartsFragment.kt
@@ -9,7 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.ArrayAdapter
-import com.example.gymapp.databinding.FragmentChartsBinding
+import android.widget.AutoCompleteTextView
+import androidx.compose.ui.platform.ComposeView
+import com.example.gymapp.R
+import com.example.gymapp.chart.CustomMarkerLabelFormatter
 import com.example.gymapp.persistence.WorkoutHistoryDatabaseHelper
 import com.example.gymapp.persistence.WorkoutSeriesDataBaseHelper
 import com.patrykandpatrick.vico.core.axis.AxisPosition
@@ -17,85 +20,86 @@ import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
 import com.patrykandpatrick.vico.core.axis.horizontal.HorizontalAxis
 import com.patrykandpatrick.vico.core.chart.layer.LineCartesianLayer
 import com.patrykandpatrick.vico.core.chart.values.AxisValueOverrider
+import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
 import com.patrykandpatrick.vico.core.model.CartesianChartModel
 import com.patrykandpatrick.vico.core.model.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.model.LineCartesianLayerModel
 import com.patrykandpatrick.vico.core.model.lineSeries
+import com.example.gymapp.chart.rememberMarker
 import com.patrykandpatrick.vico.views.chart.CartesianChartView
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class ChartsFragment : Fragment() {
 
-    private var _binding: FragmentChartsBinding? = null
-    private val binding get() = _binding!!
-
     private lateinit var historyDataBase: WorkoutHistoryDatabaseHelper
 
     private lateinit var lineChartLoad: CartesianChartView
     private lateinit var lineChartReps: CartesianChartView
+    private lateinit var customMarker: MarkerComponent
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentChartsBinding.inflate(layoutInflater, container, false)
+        return inflater.inflate(R.layout.fragment_charts, container, false).apply {
+            findViewById<ComposeView>(R.id.composeView).setContent {
+                customMarker = rememberMarker()
+            }
+            lineChartLoad = findViewById(R.id.chartViewLoad)
+            lineChartReps = findViewById(R.id.chartViewReps)
 
-        lineChartLoad = binding.chartViewLoad
-        lineChartReps = binding.chartViewReps
 
-        historyDataBase = WorkoutHistoryDatabaseHelper(requireContext(), null)
+            historyDataBase = WorkoutHistoryDatabaseHelper(requireContext(), null)
 
-        val items = historyDataBase.getExerciseNames()
+            val items = historyDataBase.getExerciseNames()
 
-        var selectedItem: String
+            var selectedItem: String
 
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
 
-        binding.exerciseSelect.apply {
-            setAdapter(adapter)
+            findViewById<AutoCompleteTextView>(R.id.exerciseSelect).apply {
+                setAdapter(adapter)
 
-            threshold = 1
+                threshold = 1
 
-            addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-                override fun afterTextChanged(s: Editable?) {
-                    if (s.toString().isEmpty()) {
-                        postDelayed({
-                            if (s.toString()
-                                    .isEmpty()
-                            ) {
-                                showDropDown()
-                            }
-                        }, 100)
+                addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int
+                    ) {
                     }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                    override fun afterTextChanged(s: Editable?) {
+                        if (s.toString().isEmpty()) {
+                            postDelayed({
+                                if (s.toString()
+                                        .isEmpty()
+                                ) {
+                                    showDropDown()
+                                }
+                            }, 100)
+                        }
+                    }
+
+                })
+
+                setOnTouchListener { _, _ ->
+                    performClick()
+                    showDropDown()
+                    false
                 }
 
-            })
-
-            setOnTouchListener { _, _ ->
-                performClick()
-                showDropDown()
-                false
+                setOnItemClickListener { parent, _, position, _ ->
+                    selectedItem = parent.adapter.getItem(position).toString()
+                    setChart(selectedItem)
+                }
             }
 
-            setOnItemClickListener { parent, _, position, _ ->
-                selectedItem = parent.adapter.getItem(position).toString()
-                setChart(selectedItem)
-            }
         }
-
-
-
-        return binding.root
     }
 
     override fun onResume() {
@@ -146,22 +150,30 @@ class ChartsFragment : Fragment() {
                     labels.getOrElse(x.toInt()) { " " }
                 }
 
-            val loadAxisValueOverrider = AxisValueOverrider.fixed(minY = minLoadValue.toFloat(), maxY = maxLoadValue.toFloat())
+            val loadAxisValueOverrider = AxisValueOverrider.fixed(
+                minY = minLoadValue.toFloat(),
+                maxY = maxLoadValue.toFloat()
+            )
             val repsAxisValueOverrider = AxisValueOverrider.fixed(maxY = maxRepValue.toFloat())
 
-            CartesianChartModel(LineCartesianLayerModel.build {  series(dataLoad.values) }, LineCartesianLayerModel.build { series(dataReps.values) })
+            CartesianChartModel(
+                LineCartesianLayerModel.build { series(dataLoad.values) },
+                LineCartesianLayerModel.build { series(dataReps.values) })
 
+            val markerFormatter = CustomMarkerLabelFormatter(repsValues)
+            customMarker.labelFormatter = markerFormatter
 
-            with(lineChartLoad){
-                (chart?.layers?.get(0) as LineCartesianLayer?)?.axisValueOverrider = loadAxisValueOverrider
+            with(lineChartLoad) {
+                (chart?.layers?.get(0) as LineCartesianLayer?)?.axisValueOverrider =
+                    loadAxisValueOverrider
                 (chart?.bottomAxis as HorizontalAxis?)?.valueFormatter = bottomAxisValueFormatter
-                //(chart?.layers?.get(1) as LineCartesianLayer?)?.axisValueOverrider = repsAxisValueOverrider
+                marker = customMarker
             }
 
-            with(lineChartReps){
-                (chart?.layers?.get(0) as LineCartesianLayer?)?.axisValueOverrider = repsAxisValueOverrider
-                (chart?.bottomAxis as HorizontalAxis?)?.valueFormatter = bottomAxisValueFormatter
-            }
+            /*           with(lineChartReps){
+                           (chart?.layers?.get(0) as LineCartesianLayer?)?.axisValueOverrider = repsAxisValueOverrider
+                           (chart?.bottomAxis as HorizontalAxis?)?.valueFormatter = bottomAxisValueFormatter
+                       }*/
 
             cartesianChartModelProducerLoad.tryRunTransaction {
                 lineSeries {
@@ -170,15 +182,15 @@ class ChartsFragment : Fragment() {
             }
 
 
-            val cartesianChartModelProducerReps = CartesianChartModelProducer.build()
-            lineChartReps.modelProducer = cartesianChartModelProducerReps
+            /* val cartesianChartModelProducerReps = CartesianChartModelProducer.build()
+             lineChartReps.modelProducer = cartesianChartModelProducerReps
 
 
-            cartesianChartModelProducerReps.tryRunTransaction {
-                lineSeries {
-                    series(dataReps.values)
-                }
-            }
+             cartesianChartModelProducerReps.tryRunTransaction {
+                 lineSeries {
+                     series(dataReps.values)
+                 }
+             }*/
         }
 
     }
@@ -189,13 +201,12 @@ class ChartsFragment : Fragment() {
         return (min - offset).toInt().roundToClosest(10)
     }
 
-    private fun getMaxValue(data: List<Float>, roundStep: Int): Int{
+    private fun getMaxValue(data: List<Float>, roundStep: Int): Int {
         val max = data.max()
         val offset = max * 0.1
-        return if (max>roundStep+roundStep/2) {
+        return if (max > roundStep + roundStep / 2) {
             (max + offset).toInt().roundToClosest(roundStep)
-        }
-        else{
+        } else {
             (max + offset).toInt().roundToClosest(1)
         }
     }
@@ -207,8 +218,4 @@ class ChartsFragment : Fragment() {
         return if (this - lower < upper - this) lower else upper
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
